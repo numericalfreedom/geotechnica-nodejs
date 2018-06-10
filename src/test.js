404d54382100b9b07414788e8db18e0273a954a6
Tata <tata@Miskolc.Nandor.Tamaskovics.Dr>
2018-06-10 22:17:33 +0200

    Files updated.

--- src/test.js
+++ src/test.js
@@ -1,49 +1,8 @@
 
+const f  = require( './lib/FleX_M2D.js' );
 
-var data = { error: undefined , result: undefined } ;
+const ff = new f.FleX_M2D ;
 
+ff.initialize() ;
 
-function solve( err , res , callback )
- {
-
-// setTimeout( callback , 100 , err , res ) ;
-
-  callback( err , res ) ;
-
- }
-
-
-function process( err , res )
- {
-
-  data.error = err ;
-  data.result = res ; 
-
-  console.log( "err= " , err , " res= " , res ) ;
-
- }
-
-
-function finish( err , res )
- {
-
-  data.error = err ;
-  data.result = res ; 
-
-  console.log( "err= " , err , " res= " , res ) ;
-
- }
-
-
-solve( 1 , 2 , process ) ;
-
-console.log( "err= " , data.error , " res= " , data.result ) ;
-
-
-solve( 2 , 1 , finish ) ;
-
-console.log( "err= " , data.error , " res= " , data.result ) ;
-
-
-console.log( process.argv ) ;
-
+ff.step() ;
