72e5c423179af2a2b47f3f993099d0faffc34cdb
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2018-06-10 17:10:27 +0200

    Data structure development

---
+++ FleX.sh
@@ -0,0 +1,11 @@
+#!/usr/share/node/head/bin/node
+
+/**
+ * FleX.sh : REPL server for the FleX explicit geotechical simulation system
+ *
+ */
+
+const repl = require('repl') ;
+
+repl.start({ prompt: 'FleX> '}) ;
+
