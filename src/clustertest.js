799894283c70dcf74cadce161ad844da613cd0d0
Numerical Freedom <numericalfreedom@googlemail.com>
2018-09-15 01:22:30 +0200

    GeoTechNiCa-2018-09-14-23-30: clustertest.js file updated.

--- src/clustertest.js
+++ src/clustertest.js
@@ -3,27 +3,65 @@
 const cluster = require( 'cluster' ) ;
 
 
+class Data
+ {
+
+  constructor( id , a , b , c )
+   {
+    this.id = id ;
+    this.a = a ;
+    this.b = b ;
+    this.c = c ;
+   }
+
+ }
+
+
 if (cluster.isMaster)
  {
 
-  const worker = cluster.fork() ;
+  const worker1 = cluster.fork() ;
+  const worker2 = cluster.fork() ;
 
-  worker.on( 'message' ,  (msg) => { console.log( msg[0][1] , msg[1][1] , msg[1][2] ) } ) ;
+  let dm1 = new Data( undefined , 1 , 2 , 3 ) ;
+  let dm2 = new Data( undefined , 4 , 5 , 6 ) ;
 
-  worker.send( [ [ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] ]  ) ;
+// worker.on( 'message' ,  (msg) => { console.log( msg[0][1] , msg[1][1] , msg[1][2] ) } ) ;
 
-  worker.disconnect() ;
+  worker1.on( 'message' , (msg) => { console.log( "Message from worker:" , msg.id , msg.a , msg.b , msg.c ) } ) ;
+  worker2.on( 'message' , (msg) => { console.log( "Message from worker:" , msg.id , msg.a , msg.b , msg.c ) } ) ;
+
+// worker.send( [ [ 1 , 2 , 3 ] , [ 4 , 5 , 6 ] ]  ) ;
+
+  worker1.send( dm1 ) ;
+  worker2.send( dm2 ) ;
+
+//  console.log( cluster ) ;
+
+  worker1.disconnect() ;
+  worker2.disconnect() ;
 
  }
 
 else if (cluster.isWorker)
  {
 
+  let dw = new Data( cluster.worker.id , 3 , 2 , 1 ) ;
+
 //  process.on( 'message' , (msg) => { console.log( msg ); } );
 
-  process.on( 'message' , (msg) => { console.log( msg[0][1] , msg[1][1] , msg[1][2] ) ; process.send( [ [ 2 , 3 , 4 ] , [ 5 , 6 , 7 ] ] ) ;  } ) ;
+//  process.on( 'message' , (msg) => { console.log( msg[0][1] , msg[1][1] , msg[1][2] ) ;
+  
+  process.on( 'message' , (msg) => { console.log( "Message from master:" , msg.id , msg.a , msg.b , msg.c ) } ) ;
 
-  process.on( 'disconnect' , () => { console.log( 'worker disconnected.' ) ; } ) ;
+//  process.send( [ [ 2 , 3 , 4 ] , [ 5 , 6 , 7 ] ] ) ;  } ) ;
+
+
+  process.on( 'disconnect' , () => { console.log( 'worker #' , cluster.worker.id , ' disconnected.' ) ; } ) ;
+
+  process.send( dw ) ;
+
+//  console.log( cluster ) ;
 
  }
 
