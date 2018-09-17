b0aa0bf1bc8b2a50306166145035a6a04d7ee993
Numerical Freedom <numericalfreedom@googlemail.com>
2018-09-18 01:07:24 +0200

    GeoTechNiCa-2018-09-17-23-00: clustereventtest.js file modified.

--- src/clustereventtest.js
+++ src/clustereventtest.js
@@ -1,19 +1,131 @@
+/*
+
+https://stackoverflow.com/questions/41423905/wait-for-several-web-workers-to-finish
+
+*/
+
+
+/*
+
+When the last piece of data is received, call your compute-the-final-solution function:
+
+var data = [];
+
+function createWorker(i) {
+    var v = new Worker('js/worker.js');
+    v.postMessage(i);
+    v.onmessage = function(event){
+        data.push(event.data);
+        if (data.length === 100) {               // <====
+            computeFinalSolution();              // <====
+        }                                        // <====
+    };
+}
+
+for(var i = 0; i < 100; i++) {
+    createWorker(i);
+
+}
+
+*/
+
+
+/*
+
+Obviously, parameterize that as you consider necessary, but createWorker isn't currently parameterized other than i, so...
+
+Note that the entries in data may not be in order. The worker for i == 0 might not complete until after the worker for i == 1, just because of the vagaries of thread scheduling or if the work requires more processing. If you need them in order, it's easily done, but we have to add a counter (or loop through data on each completion to check):
+
+var data = [];
+var dataReceived = 0;
+
+function createWorker(i) {
+    var v = new Worker('js/worker.js');
+    v.postMessage(i);
+    v.onmessage = function(event){
+        data[i] = event.data;                    // <====
+        if (++dataReceived === 100) {            // <====
+            computeFinalSolution();              // <====
+        }                                        // <====
+    };
+}
+
+for(var i = 0; i < 100; i++) {
+    createWorker(i);
+}
+
+*/
+
+
+/*
+
+If you want a more modern, flexible approach, consider using promises, which are native to JavaScript as of ES2015 (aka ES6) and can be polyfilled for use on older JavaScript engines:
+
+function createWorker(i) {
+    return new Promise(function(resolve, reject) {
+        var v = new Worker('js/worker.js');
+        v.postMessage(i);
+        v.onmessage = function(event){
+            // If you report errors via messages, you'd have a branch here for checking
+            // for an error and either calling `reject` or `resolve` as appropriate.
+            resolve(event.data);
+        };
+        // EITHER:
+        v.onerror = reject; // Rejects the promise if an error is raised by the web worker, passing along the ErrorEvent
+        // OR:
+        v.onerror = function(event) {
+            // Rejects the promise using the error associated with the ErrorEvent
+            reject(event.error);
+        };
+    });
+}
+
+var promises = [];
+for(var i = 0; i < 100; i++) {
+    promises.push(createWorker(i));
+}
+Promise.all(promises)
+    .then(function(data) {
+        // `data` has the results, compute the final solution
+    })
+    .catch(function(error) {
+        // something went wrong
+    });
+
+*/
+
+
+/*
+
+You could promisify the workers and then use Promise.all:
+
+Promise.all(Array.from(Array(100), (x, i) => i).map(i =>
+  new Promise((resolve, reject) => {
+    const worker = new Worker('js/worker.js');
+    worker.postMessage(i);
+    worker.addEventListener('message', event => resolve(event.data));
+    worker.addEventListener('error', reject);
+  }))
+  .then(results => ...);
+
+*/
+
 
 const cluster      = require( 'cluster' ) ;
 const EventEmitter = require( 'events' ) ;
 const sleep        = require( '/usr/share/node/head/lib/node_modules/sleep' );
+const systemsleep  = require( '/usr/share/node/head/lib/node_modules/system-sleep' );
 
 
-var v  = new Array( 16 ) ;
+var v  = new Array( 32 ) ;
 var iv = 0 ;
 
 
 function vector( msg )
  {
 
+  console.log( "Message from worker:" , msg.id , msg.a , msg.b , msg.c , msg.r ) ;
   v[msg.id] = msg.r ;
-
-// console.log( v ) ;
 
   ++iv ;
 
@@ -57,14 +169,14 @@ if( cluster.isMaster )
  {
 
   let i  = undefined ;
-  let ic = 16 ;
+  let ic = 32 ;
   let iw = undefined ;
 
   const ee = new EventEmitter() ;
   const pm = new Promise( (resolve , reject) => {} ) ;
 
 
-  ee.on( 'wait' , () => {} ) ;
+  ee.on( 'wait' , () => { console.log( v ); } ) ;
 
 
   const workerpool = new Array( 4 ) ;
@@ -105,12 +217,16 @@ if( cluster.isMaster )
    } ; // end if()
 
 
-// while( iv < 16 )  ee.emit( 'wait' ) ;
+//  ee.emit( 'wait' ) ;
 
-  setTimeout( () => { console.log( v ); } , 2000 ) ;
+
+    setTimeout( () => { console.log( v ) ; } , 1000 ) ;
 
 
 //  console.log( v ) ;
+
+
+//  while( iv < 16 )  console.log( iv );
 
 
   for( i = 0; i < workerpool.length; ++i )
