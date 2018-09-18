41add97d902ae709f81278dd2ec6d6bd19fb00be
Numerical Freedom <numericalfreedom@googlemail.com>
2018-09-19 01:56:16 +0200

    GeoTechNiCa-2018-09-18-23-45: clusterpromisetest.js file updated.

--- src/clusterpromisetest.js
+++ src/clusterpromisetest.js
@@ -52,12 +52,14 @@ if( cluster.isMaster )
  {
 
   let i  = undefined ;
-  let ic = 16 ;
+  let ic =   16 ;
   let iw = undefined ;
-  let ip =  4 ;
+  let ip =    4 ;
 
+  let j  = undefined ;
 
-  let promisepool = new Array( ic ) ;
+	 
+  let promisepool = new Array( ip ) ;
 
   let dm          = new Data( undefined , undefined , undefined , undefined ) ;
 
@@ -76,6 +78,8 @@ if( cluster.isMaster )
 
     cluster.workers[iw].send( dm ) ;
 
+    console.log( 'i=' , i , 'iw=' , iw ) ;
+
    } ;
 
 
@@ -86,39 +90,37 @@ if( cluster.isMaster )
 
       cluster.workers[iw].on( 'message' , (msg) => {
 
-	if( msg.id == i )
-	 {
+        console.log( "Message from worker:" , msg.id , msg.a , msg.b , msg.c , msg.r ) ;
 
-          console.log( "Message from worker:" , msg.id , msg.a , msg.b , msg.c , msg.r ) ;
-
-	  resolve( msg.r ) ;
-
-         } ;
+        if( msg.id == i )  resolve( msg.r ) ;
       
        } ) ;
 
      } ) ;
-
-//  cluster.workers[iw].removeListener( 'message' ) ;
 
     return( p ) ;
 
    } ;
 
 
-  for( i = 0 , iw = 1; i < ic; ++i , iw %= ip , ++iw )
+  for( i = 0; i < ic; i += ip )
    {
 
-    console.log( 'i=' , i , 'iw=' , iw ) ;
+    for( j = 0 , iw = 1; j < ip; ++j , ++iw )
+     {
 
-    sendData( i , iw ) ;
+      sendData( i+j , iw ) ;
 
-    promisepool[i] = receiveData( i , iw ) ;
+      promisepool[j] = receiveData( i+j , iw ) ;
+    
+     } ;
+     
+    Promise.all( promisepool ).then( (v) => { console.log( v ) } ) ;
 
    } ;
 
 
-  Promise.all( promisepool ).then( (v) => { console.log( v ) } ) ;
+// Promise.all( promisepool ).then( (v) => { console.log( v ) } ) ;
 
 
   for( i=1; (i <= ip); ++i )  cluster.workers[i].disconnect() ;
@@ -133,6 +135,8 @@ else if ( cluster.isWorker )
   let dw = new Data( cluster.worker.id , 3 , 2 , 1 ) ;
 
   process.on( 'message' , (msg) => { dw.id = msg.id; dw.a = msg.a; dw.b = msg.b; dw.c = msg.c; dw.r = Data.run( dw.a , dw.b , dw.c ); process.send( dw ); console.log( "Message from master:" , msg ) } ) ;
+
+// process.on( 'message' , (msg) => { dw.id = msg.id; dw.a = msg.a; dw.b = msg.b; dw.c = msg.c; dw.r = Data.run( dw.a , dw.b , dw.c ); process.send( dw ); } ) ;
 
   process.on( 'disconnect' , () => { console.log( 'worker #' , cluster.worker.id , ' disconnected.' ) ; } ) ;
 
