66c586eaa1f66c3090973d4fa106291f728e1ec6
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2018-10-30 22:46:06 +0100

    GeoTechNiCa-2018-10-30-22-45: promisetest.js file created.

---
+++ src/promisetest.js
@@ -0,0 +1,51 @@
+const event = require( 'events' ) ;
+
+
+const emitter = new event ;
+
+
+function wait( length )
+ {
+ 
+  setTimeout( () => { emitter.emit( 'resolve' ); } , length ) ;
+ 
+ }
+
+
+function solve()
+ {
+ 
+  const promise = new Promise( (resolve,reject) => {
+
+     emitter.once( 'resolve'  , (data) => { resolve( Math.random() )
+
+     } )
+
+     console.log( emitter )
+  
+   } )
+
+  return( promise )
+
+ }
+
+
+promisepool = [ solve() , solve() , solve() , solve() ] 
+
+
+Promise.all( promisepool ).then( (data) => { console.log( 'finished:' , data ) 
+
+} )
+
+
+console.log( promisepool )
+
+
+// wait( 2000 ) ;
+
+
+// emitter.emit( 'resolve' ) ;
+
+
+
+
