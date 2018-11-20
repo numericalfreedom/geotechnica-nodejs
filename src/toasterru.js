90366ffefa2ff9544a857c769c5a566f3fbbda3a
Master <master@marabou.ifgt.tu-freiberg.de>
2018-11-20 13:18:06 +0100

    GeoTechNiCa-2018-11-30-13-00: toasterru.js file modified.

--- src/toasterru.js
+++ src/toasterru.js
@@ -1,9 +1,7 @@
 
 const Async = require('async')
 
-
 var proxies = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16 }
-
 
 async function process () {
   var promise = new Promise((resolve, reject) => {
@@ -16,17 +14,16 @@ async function process () {
   await promise
 }
 
-
 function displayitems (item, key, callback) {
-// callback()
+  console.log(callback)
   process()
   console.log('key : ' + key + '')
   console.log('item : ' + item + '')
   callback()
 }
 
-
 async function asyncdisplayitems (item, key, callback) {
+  console.log(callback)
   var promise = new Promise((resolve, reject) => {
     var timeout = (3000 + (500 * Math.random()))
     setTimeout(() => {
@@ -37,10 +34,8 @@ async function asyncdisplayitems (item, key, callback) {
     }, timeout)
   })
   await promise
-// callback()
   return (promise)
 }
-
 
 function finalcallback (err) {
   if (err) {
@@ -49,11 +44,8 @@ function finalcallback (err) {
   console.log('all done')
 }
 
-
-
-Async.eachOf(proxies, asyncdisplayitems, finalcallback)
+// Async.eachOf(proxies, asyncdisplayitems, finalcallback)
 
 // Async.eachOfSeries(proxies, asyncdisplayitems, finalcallback)
 
-// Async.eachOfLimit(proxies, 5, asyncdisplayitems, finalcallback)
-
+Async.eachOfLimit(proxies, 3, asyncdisplayitems, finalcallback)
