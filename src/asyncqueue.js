99e98e9f4d9a5a212c9fc52edf87288c5701fa0e
Numerical Freedom <numericalfreedom@googlemail.com>
2018-11-20 23:01:31 +0100

    GeoTechNiCa-2018-11-30-21-00: toasterru.js file modified and async queue successfully added.

---
+++ src/asyncqueue.js
@@ -0,0 +1,93 @@
+
+const Async = require('async')
+
+var proxies = { a: 1, b: 2, c: 3, d: 4, e: 5, f: 6, g: 7, h: 8, i: 9, j: 10, k: 11, l: 12, m: 13, n: 14, o: 15, p: 16 }
+
+async function process () {
+  var promise = new Promise((resolve, reject) => {
+    var timeout = (5000 * Math.random())
+    setTimeout(() => {
+      console.log('timeout=', timeout)
+      resolve(timeout)
+    }, timeout)
+  })
+  await promise
+}
+
+function displayitems (item, key, callback) {
+//console.log(callback)
+  process()
+  console.log('key : ' + key + '')
+  console.log('item : ' + item + '')
+  callback()
+}
+
+
+function queuedisplayitems (item, callback) {
+    var timeout = (3000 + (500 * Math.random()))
+    setTimeout(() => {
+      console.log('timeout=', timeout)
+      console.log('item : ' + item + '')
+      callback()
+    }, timeout)
+}
+
+
+function promisedisplayitems (item, callback) {
+//console.log(callback)
+  var promise = new Promise((resolve, reject) => {
+    var timeout = (3000 + (500 * Math.random()))
+    setTimeout(() => {
+      console.log('timeout=', timeout)
+      console.log('item : ' + item + '')
+      resolve(timeout)
+      callback()
+    }, timeout)
+  })
+  return (promise)
+}
+
+
+async function asyncdisplayitems (item, key, callback) {
+//console.log(callback)
+  var promise = new Promise((resolve, reject) => {
+    var timeout = (3000 + (500 * Math.random()))
+    setTimeout(() => {
+      console.log('timeout=', timeout)
+      console.log('key : ' + key + '')
+      console.log('item : ' + item + '')
+      resolve(timeout)
+    }, timeout)
+  })
+  await promise
+  return (promise)
+}
+
+function finalcallback (err) {
+  if (err) {
+    console.log('error')
+  }
+  console.log('all done')
+}
+
+// Async.eachOf(proxies, asyncdisplayitems, finalcallback)
+
+// Async.eachOfSeries(proxies, asyncdisplayitems, finalcallback)
+
+// Async.eachOfLimit(proxies, 3, asyncdisplayitems, finalcallback)
+
+
+var q = Async.queue(queuedisplayitems, 3)
+
+q.drain = () => { console.log( 'Alli items have been processed.' ) }
+
+
+let i = undefined
+
+for( i = 0; i<10; ++i ) {
+
+  q.push( i , (err) => { console.log( 'A done' ) } )
+
+}
+
+
