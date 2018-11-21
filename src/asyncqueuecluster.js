6f869a6e64676b7273b1e9d224aae6c3140c1846
Numerical Freedom <numericalfreedom@googlemail.com>
2018-11-21 22:15:43 +0100

    GeoTechNiCa-2018-11-21-20-15: asyncqueuecluster.js modified.

--- src/asyncqueuecluster.js
+++ src/asyncqueuecluster.js
@@ -1,7 +1,8 @@
+'use strict'
 
-const Async   = require('async')
+const Async = require('async')
 const Cluster = require('cluster')
-const Event   = require('events')
+const Event = require('events')
 
 function disconnect (worker) {
   console.log(`Worker #${worker.id} has disconnected.`)
@@ -12,118 +13,116 @@ class Point {
     this.x = x
     this.y = y
     this.z = z
-  }
-}
-
-var i = undefined
-var j = undefined
-
-
-const nw = 4
-const np = 8 
-
-const worker  = new Array(nw)
-const emitter = new Event()
-
-
-const points  = new Array(np)
-
-for (i = 0; i < np; ++i) {
-  points[i] = new Point(Math.random(), Math.random(), Math.random())
-}
-
-
-function workercompute (i,callback) {
-
-  var k = undefined
-
-  for( k = 0; ((k < nw) && worker[k].status); ++k ); 
-
-  worker[k].send( {'id': i , 'obj': points[i] } )
-
-  console.log( 'Send:', i, points[i] )
-
-  let promise = new Promise ((resolve, reject) => {
-
-    console.log ( 'Emitter.once' , i.toString())
-
-    emitter.once (msg.id.toString(), (msg) => {
-	    
-      console.log ('Message received:', msg.id, msg.obj, msg.rst)
-
-      resolve (msg.rst)
-
-      callback()
-
-    })
-	  
-  })
-
-
-  return (promise)
-
-}
-
-
-if (Cluster.isMaster) {
-
-
-  var i = undefined
-
-
-  for (i = 0; i < nw; ++i) {
-
-    worker[i] = Cluster.fork()
-
-    worker[i].status = false
-
-    worker[i].on ('message', (msg) => {
-
-      console.log ( 'Emit:', msg.id.toString(), msg)
-
-      emitter.emit (msg.id.toString(), msg)
-
-    })
-
+    this.r = 0.0
   }
 
-
-  var q = Async.queue(workercompute, nw)
-
-  q.drain = () => { console.log( 'Alli items have been processed.' ) }
-
-
-  for( i = 0; i<np; ++i ) {
-
-    q.push( i , (err) => { console.log( 'A done' ) } )
-
-  }
-
-
-  for (i = 0; i < nw; ++i) {
-    worker[i].disconnect()
-  }
-
-
-  Cluster.on('disconnect', disconnect)
-
-
-} else if (Cluster.isWorker) {
-
-  process.on ('message', (msg) => {
-
-    var ix = (1e7 + (Math.random() * 1e7))
+  static compute () {
+    var ix = (1e6 + (Math.random() * 1e6))
 
     var result = undefined
 
     for (var i = 0; i < ix; ++i) {
-
       result = (Math.sin(i) + Math.cos(i))
-
     }
 
-   process.send ({ 'id': Cluster.worker.id, 'obj': msg, 'rst': result })
-	  
+    return (result)
+  }
+}
+
+const nw = 4
+const np = 24
+
+const worker = new Array(nw)
+const emitter = new Event()
+
+const points = new Array(np)
+
+for (let i = 0; i < np; ++i) {
+  points[i] = new Point(Math.random(), Math.random(), Math.random())
+}
+
+function workercompute (j, callback) {
+  var k = 0
+
+  while (worker[k].status) {
+    ++k
+  }
+
+  worker[k].status = true
+
+  console.log('Send:', { 'w': k, 'j': j, 'obj': points[j] })
+
+  worker[k].send({ 'w': k, 'j': j, 'cmd': 'compute', 'obj': points[j] })
+
+  let promise = new Promise((resolve, reject) => {
+    console.log('Emitter.once', k.toString())
+
+    emitter.once(j.toString(), (msg) => {
+      console.log('Message received:', msg.w, msg.j, msg.obj, msg.rst)
+
+      worker[msg.w].status = false
+
+      points[msg.j].r = msg.rst
+
+      resolve(msg.rst)
+
+      callback()
+    })
   })
 
+  return (promise)
+}
+
+if (Cluster.isMaster) {
+  var i = undefined
+  var j = undefined
+  var result = undefined
+
+  for (i = 0; i < nw; ++i) {
+    worker[i] = Cluster.fork()
+
+    worker[i].status = false
+
+    worker[i].on('message', (msg) => {
+      console.log('Emit:', msg.j.toString(), msg)
+
+      emitter.emit(msg.j.toString(), msg)
+    })
+  }
+
+  var q = Async.queue(workercompute, nw)
+
+  q.drain = function () {
+    console.log('All items have been processed.')
+
+    for (i = 0; i < nw; ++i) {
+      worker[i].disconnect()
+    }
+
+    for (i = 0; i < np; ++i) {
+      console.log(points[i])
+    }
+  }
+
+  for (i = 0; i < np; ++i) {
+    console.log('i=', i)
+
+    q.push(i, (err) => {
+      if (err) { console.log('Error:', err) } else { console.log('Done.') }
+    })
+  }
+
+  Cluster.on('disconnect', disconnect)
+} else if (Cluster.isWorker) {
+  process.on('message', (msg) => {
+    switch (msg.cmd) {
+      case ('compute'):
+
+        var result = Point.compute()
+
+        break
+    }
+
+    process.send({ 'w': msg.w, 'j': msg.j, 'cmd': msg.cmd, 'obj': msg.obj, 'rst': result })
+  })
 }
