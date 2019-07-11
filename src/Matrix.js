bb269677bd26774c053d688e98de5d27aaf6cae3
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2019-07-11 22:11:22 +0200

    Matrix.js updated.

--- src/Matrix.js
+++ src/Matrix.js
@@ -779,82 +779,61 @@ function inv()
   let i   = undefined ;
   let j   = undefined ;
   let k   = undefined ;
-  let bii = undefined ;
-  let bji = undefined ;
+  let akk = undefined ;
+  let akj = undefined ;
+  let aik = undefined ;
+
 
   if( this.d )
   
-    for( i = 0;  i < this.nr;  ++i )
-
-      if( bii = this.v[ this.idx( i , i ) ] )
-       {
-    
-        for( k = 0;  k < this.nc; ++k )
-
-          if( (this.idx( i , k ) < this.nv) && (this.v[ this.idx( i , k ) ]) )
-
-            this.v[ this.idx( i , k ) ] /= bii ;
-
-        this.v[ this.idx( i , i ) ] = ( 1.0 / bii ) ;
-
-        for( j = 0;  j < this.nr;  ++j )
-        
-          if( j != i )
-           {
-
-            bji = this.v[ this.idx( j , i ) ] ;
-
-            for( k = 0;  k < this.nc;  k++ )
-
-              if( (this.idx( i , k ) < this.nv) && (this.idx( j , k ) < this.nv) && (this.v[ this.idx( j , k ) ]) )
-
-                this.v[ this.idx( j , k ) ] -= ( bji * this.v[ this.idx( i , k ) ] ) ;
-
-            if( this.idx( j , i ) < this.nv )
-
-              this.v[ this.idx( j , i ) ] = (- bji / bii ) ;
-
-           } ; // end if-
-
-       } // end if +
-
-      else
-
-        for( k = (i + 1);  k < this.nc;  this.v[ this.idx( i , k++ ) ] = undefined ) ;
 
   else
 
-    for( i = 0;  i < this.nr;  ++i )
 
-      if( bii = this.v[ this.idx( i , i ) ] )
+    for( k = 0;  k < this.nr;  ++k )
+
+      if( akk = this.v[ this.idx( k , k ) ] )
        {
 
-        for( k = 0;  k < this.nc;  this.v[ this.idx( i , k ) ] /= bii , k++ ) ;
+        for( i = 0;  i < this.nc;  ++i )
 
-        this.v[ this.idx( i , i ) ] = ( 1.0 / bii ) ;
+          for( j = 0;  j < this.nr;  ++j )
+
+            if( (i != k) && (j != k) )
+             {
+
+              akj = this.v[ this.idx( k , j ) ] ;
+
+              aik = this.v[ this.idx( i , k ) ] ;
+
+              this.v[ this.idx( i , i ) ] -= ( (akj * aik) / akk ) ;
+
+             } ;
+
+
+        for( i = 0;  i < this.nc;  ++i )
+
+          if( i != k )
+
+            this.v[ this.idx( i , k ) ] /= (- akk ) ;
+
 
         for( j = 0;  j < this.nr;  ++j )
 
-          if( j != i )
-           {
+          if( k != j )
 
-            bji = this.v[ this.idx( j , i ) ] ;
+            this.v[ this.idx( k , j ) ] /= akk ;
 
-            for( k = 0;  k < this.nc;  k++ )
 
-              if( this.v[ this.idx( j , k ) ] && (k != i) )
+        this.v[ this.idx( k , k ) ] = ( 1.0 / akk ) ;
 
-                this.v[ this.idx( j , k ) ] -= ( bji * this.v[ this.idx( i , k ) ] ) ;
 
-            this.v[ this.idx( j , i ) ] = (- bji / bii ) ;
-
-           } ; // end if-
-
-       } // end if +
+       } // end if{} +
 
       else
 
-        for( k = 0;  k < this.nc;  this.v[ this.idx( i , k++ ) ] = undefined ) ;
+        this.v[ this.idx( k , k ) ] = undefined ;
+
 
   return ;
 
