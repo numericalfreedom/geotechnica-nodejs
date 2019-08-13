281a46060696615adf321ae6bc5a7642a6efb048
Numerical Freedom <numericalfreedom@googlemail.com>
2019-08-13 21:44:08 +0200

    TaylorD.js updated.

---
+++ src/sphere.js
@@ -0,0 +1,29 @@
+
+
+
+
+
+
+class Sphere
+ {
+ 
+   constructor( rs , rr ,  r , a , v , u )
+    {
+
+      this.rr = rr ;
+      this.r  = r  ;
+      this.a = a ;
+      this.v = v ;
+      this.u = u ;
+    
+    }
+
+ }
+
+
+class particleSystem
+ {
+
+  constructor( n )
+
+ }
