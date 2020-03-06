96b31477d2283736d156d372d2628896f3909d9c
Numerical Freedom <numericalfreedom@googlemail.com>
2020-03-06 16:40:50 +0100

    src updated.

--- src/lsdynamat.js
+++ src/lsdynamat.js
@@ -2,88 +2,7 @@
 
 const fs = require( 'fs' ) ;
 
-module.exports = { lsdynamat , runlsdynamat , lsdynamat005 , biota , bishopb , ctu , ctsf , rrsf , ctg , rrg , ctm , rrm } ;
-
-const mid  =  0 ;
-const ro   =  1 ;
-const g    =  2 ;
-const kun  =  3 ;
-const a0   =  4 ;
-const a1   =  5 ;
-const a2   =  6 ;
-const pc   =  7 ;
-const vcr  =  8 ;
-const ref  =  9 ;
-const lcid = 10 ;
-const e1   = 11 ;
-const e2   = 12 ;
-const e3   = 13 ;
-const e4   = 14 ;
-const e5   = 15 ;
-const e6   = 16 ;
-const e7   = 17 ;
-const e8   = 18 ;
-const e9   = 19 ;
-const e10  = 20 ;
-const p1   = 21 ;
-const p2   = 22 ;
-const p3   = 23 ;
-const p4   = 24 ;
-const p5   = 25 ;
-const p6   = 26 ;
-const p7   = 27 ;
-const p8   = 28 ;
-const p9   = 29 ;
-const p10  = 30 ;
-
-
-const fd = fs.openSync( 'mat.key' , 'w' ) ;
-
-var pv = new Array( 31 ) ;
-
-var i  = undefined ;
-
-pv[mid]  = 'M5_123456' ;
-pv[ro]   =  1.800e-9 ;
-pv[g]    =  6.385e+1 ;
-pv[kun]  =  3.000E+4 ;
-pv[a0]   =  3.400E-3 ;
-pv[a1]   =  7.033E-2 ;
-pv[a2]   =  3.000E-1 ;
-pv[pc]   = -6.900E-3 ;
-
-pv[vcr]  =  0.0 ;
-pv[ref]  =  0.0 ;
-
-pv[lcid] = 'M5_123456' ;
-
-pv[e1]   =  0.00    ;
-pv[e2]   = -1.04E-1 ;
-pv[e3]   = -1.61E-1 ;
-pv[e4]   = -1.92E-1 ;
-pv[e5]   = -2.24E-1 ;
-pv[e6]   = -2.46E-1 ;
-pv[e7]   = -2.71E-1 ;
-pv[e8]   = -2.83E-1 ;
-pv[e9]   = -2.90E-1 ;
-pv[e10]  = -4.00E-1 ;
-
-pv[p1]   =   0.00   ;
-pv[p2]   =  2.00E+1 ;
-pv[p3]   =  4.00E+1 ;
-pv[p4]   =  6.00E+1 ;
-pv[p5]   =  1.20E+2 ;
-pv[p6]   =  2.00E+2 ;
-pv[p7]   =  4.00E+2 ;
-pv[p8]   =  6.00E+2 ;
-pv[p9]   =  8.00E+2 ;
-pv[p10]  =  4.10E+3 ;
-
-
-mat_005( fd , pv , pv ) ;
-
-
-fs.closeSync( fd ) ; 
+module.exports = { lsdynamat , runlsdynamat , lsdynamat005 , dimension , biota , bishopb , ctu , ctsf , rrsf , ctg , rrg , ctm , rrm } ;
 
 
 /**
@@ -193,7 +112,7 @@ function runlsdynamat()
   this.ns   = this.nzs ;
   this.nf   = this.nzf ;
   this.ng   = this.nzg ;
-
+  
   this.n    = ( this.nf + this.ng ) ;
   this.s    = ( this.nf / this.n  ) ;
 
@@ -252,11 +171,11 @@ function runlsdynamat()
       this.eps += ( this.deps = (this.ctu * this.dpt) ) ;
 
 
-      this.ns  += ( this.dns  = (- ((1.0 - this.n) * this.cts) - (this.cts * this.dpe)) - (this.ns * this.deps) ) ;
+      this.ns  += ( this.dns  = (((- (this.ns * this.cts * this.dpn)) - (this.cts * this.dpe) - (this.ns * this.deps) )) ) ;
 
-      this.nf  += ( this.dnf  = (- this.nf * this.ctf * this.dpn) - (this.nf * this.deps) ) ;
+      this.nf  += ( this.dnf  = (((- (this.nf * this.ctf * this.dpn)) - (this.nf * this.deps))) ) ;
 
-      this.ng  += ( this.dng  = (- this.ng * this.ctg * this.dpn) - (this.ng * this.deps) ) ;
+      this.ng  += ( this.dng  = (((- (this.ng * this.ctg * this.dpn)) - (this.ng * this.deps))) ) ;
 
 
       this.n    = ( this.nf + this.ng ) ;
@@ -278,6 +197,7 @@ function runlsdynamat()
       this.rf   = ( (this.nzf * this.rzf) / this.nf ) ;
       
       this.rg   = ( (this.nzg * this.rzg) / this.ng ) ;
+      
       
       this.r    = ( this.rz / (this.ns + this.nf + this.ng) ) ;
 
@@ -306,20 +226,23 @@ function runlsdynamat()
 
 /**
  *
- * function lsdynamat005 : MAT_005 record
+ * function dimension : dimension transformation
  *
- * @param {string} fn  - File name
- * @param {Array}  rvs - Short array of results for material record
- * @param {Array}  rvl - Long array of results for load curve
+ * @param {number} mr  - mass ratio
+ * @param {number} mx  - mass exponent 
+ * @param {number} lr  - length ratio
+ * @param {number} lx  - length exponent 
+ * @param {number} tr  - time ratio
+ * @param {number} tx  - time exponent 
  *
  */
 
-function lsdynamat005( fn , rvs , rvl )
+function dimension( mr , mx , lr , lx , tr , tx )
  {
 
-  return ;
+  return( Math.pow( mr , mx ) * Math.pow( lr , lx ) * Math.pow( tr , tx ) ) ;
 	 
- }
+ } ; // end function dimension()
 
 
 /**
@@ -529,62 +452,112 @@ function rrm( rz , vm , wm , pe , pr )
 
 /**
  *
- * @function mat_005
+ * @function lsdynamat005
  *
- * @param r
- * @param c
- * @param k
- * @param p
- * @param pz
+ * @param fn
+ * @param pv
+ * @param rvs
+ * @param rvl
  *
  */
 
-function mat_005( fd , pv , px )
+function lsdynamat005( fn , pv , rvs , rvl )
  {
 
-  let i  = undefined ;
-  let ix = pv.length ;
+  const mid  =  0 ;
+  const ro   =  1 ;
+  const g    =  2 ;
+  const kun  =  3 ;
+  const a0   =  4 ;
+  const a1   =  5 ;
+  const a2   =  6 ;
+  const pc   =  7 ;
+  const vcr  =  8 ;
+  const ref  =  9 ;
+  const lcid = 10 ;
 
-  let ps = new Array( ix ) ;
+  let i      = undefined ;
+  let j      = undefined ;
+  let k      = undefined ;
 
-  ps[mid] = `${pv[mid]}` ;
+  const ipv  = pv.length ;
+  const isv  = rvs.length ;
+  const ilv  = rvl.length ;
 
-  for( i = 0 ; i < ix ; ++i )
+  const ipn  = 0 ;
+  const ipx  = ipv ;
+
+  const isn  = ( ipx ) ;
+  const isx  = ( ipv + isv ) ;
+
+  const iln  = ( isx ) ;
+  const ilx  = ( isx + ilv ) ;
+
+ 
+  let ps = new Array( ilx ) ;
+  
+  for( i = 0 ; i < ilx ; ps[i++] = [ undefined , undefined ] ) ;
+
+
+  for( i = ipn ; i < ipx ; ++i )
 
     switch( i )
      {
 
       case mid:
 
-       ps[mid] = `${pv[mid]}` ;
+        ps[mid][0] = `${pv[mid]}` ;
 
-       break ;
+        break ;
 
       case vcr:
       case ref:
 
-       ps[i] = `        ${pv[i]}` ;
+        ps[i][0] = `        ${pv[i]}` ;
 
       case lcid:
 
-       ps[lcid] = `${pv[lcid]}` ;
+        ps[lcid][0] = `${pv[lcid]}` ;
 
-       break ;
+        break ;
 
       default:
 
-       if( pv[i] >= 0 )
+        if( pv[i] >= 0 )
 
-         ps[i] = pv[i].toExponential(4) ;
+          ps[i][0] = pv[i].toExponential(4) ;
 
-       else
+         else
 
-	 ps[i] = pv[i].toExponential(3) ;
+  	      ps[i][0] = pv[i].toExponential(3) ;
 
-       break ;
+        break ;
 
      } ; // end switch()
-	 
+
+
+  for( i = isn , j = 0 ; i < isx ; ++i , j = (i % 2) )
+ 
+    if( rsv[i-isn][j] >= 0 )
+
+      ps[i] = rsv[i-isn][j].toExponential(4) ;
+
+     else
+
+      ps[i] = rsv[i-isn][j].toExponential(3) ;
+
+
+  for( i = iln , j = 0 ; i < ilx ; ++i , j = (i % 2) )
+ 
+    if( rlv[i-iln][j] >= 0 )
+
+      ps[i] = rlv[i-iln][j].toExponential(4) ;
+
+     else
+
+      ps[i] = rlv[i-iln][j].toExponential(3) ;
+
+  const fd = fs.openSync( 'mat.key' , 'w' ) ;
 
   fs.writeSync( fd , '$ *MAT_005 \n' ) ;
   fs.writeSync( fd , '*MAT_SOIL_AND_FOAM \n' ) ;
@@ -609,18 +582,23 @@ function mat_005( fd , pv , px )
   fs.writeSync( fd , '$ \n' ) ;
   fs.writeSync( fd , '$      VCR       REF      LCID \n' ) ;
   fs.writeSync( fd , ` ${ps[vcr]} ${ps[ref]} ${ps[lcid]} \n` ) ;
-  fs.writeSync( fd , '$ \n' ) ;
+  fs.writeSync( fd , '$ \n' ) ;  
+
+  i = isn ;
+
   fs.writeSync( fd , '$     EPS1      EPS2      EPS3      EPS4      EPS5      EPS6      EPS7      EPS8  \n' ) ;
-  fs.writeSync( fd , ` ${ps[e1]} ${ps[e2]} ${ps[e3]} ${ps[e4]} ${ps[e5]} ${ps[e6]} ${ps[e7]} ${ps[e8]} \n` ) ;
+  fs.writeSync( fd , ` ${ps[i]} ${ps[i+=isx]} ${ps[e3]} ${ps[e4]} ${ps[e5]} ${ps[e6]} ${ps[e7]} ${ps[e8]} \n` ) ;
   fs.writeSync( fd , '$ \n' ) ;
   fs.writeSync( fd , '$     EPS9     EPS10 \n' ) ;
   fs.writeSync( fd , ` ${ps[e9]} ${ps[e10]} \n` ) ;
   fs.writeSync( fd , '$ \n' ) ;
+
   fs.writeSync( fd , '$       P1        P2        P3        P4        P5        P6        P7        P8  \n' ) ;
   fs.writeSync( fd , ` ${ps[p1]} ${ps[p2]} ${ps[p3]} ${ps[p4]} ${ps[p5]} ${ps[p6]} ${ps[p7]} ${ps[p8]} \n` ) ;
   fs.writeSync( fd , '$ \n' ) ;
   fs.writeSync( fd , '$       P9       P10 \n' ) ;
   fs.writeSync( fd , ` ${ps[p9]} ${ps[p10]} \n` ) ;
+
   fs.writeSync( fd , '$ \n' ) ;
   fs.writeSync( fd , '$...............................................................................  \n' ) ;
   fs.writeSync( fd , '$ \n' ) ;
@@ -644,8 +622,10 @@ function mat_005( fd , pv , px )
   fs.writeSync( fd , '$ \n' ) ;
   fs.writeSync( fd , '$...............................................................................  \n' ) ;
 
+  fs.closeSync( fd ) ; 
+
   return ;
 
- } ; // end function()
+ } ; // end function lsdynamat005()
 
 
---
+++ src/lsdynamat005.js
@@ -0,0 +1,68 @@
+"use strict" ;
+
+const short = true ;
+
+const lsdynamat  = require( './lsdynamat' ) ;
+
+const mat005 = new lsdynamat.lsdynamat() ;
+
+const mr    =    0.001 ;
+const lr    = 1000.000 ;
+const tr    =    1.000 ;
+
+const rmx   =    1.0 ;
+const rlx   = (- 3.0 ) ;
+const rtx   = (  0.0 ) :
+const rdtf  = mat005.dimension( mr , rmx , lr , rlx , tr , rtx ) ;
+
+const smx   =    1.0 ;
+const slx   = (- 1.0 ) ;
+const stx   = ( -2.0 ) :
+const sdtf  = mat005.dimension( mr , smx , lr , slx , tr , stx ) ;
+
+const a0mx  =    2.0 ;
+const a0lx  = (- 2.0 ) ;
+const a0tx  = ( -4.0 ) :
+const a0dtf = mat005.dimension( mr , a0mx , lr , a0lx , tr , a0tx ) ;
+
+const a1mx  =    1.0 ;
+const a1lx  = (- 1.0 ) ;
+const a1tx  = ( -2.0 ) :
+const a1dtf = mat005.dimension( mr , a1mx , lr , a1lx , tr , a1tx ) ;
+
+const rzsn = 10 ;
+const rzn  = 1800.00 ;
+const rzx  = 2100.00 ;
+const rzs  = ( (rzx - rzn) / rzsn ) ;
+
+const szsn = 10 ;
+const szn  =    0.00 ;
+const szx  =    1.00 ;
+const szs  = ( (szx - szn) / szsn ) ;
+
+const [ rs , rl ]  = mat005.lsdynamat005() ;
+
+
+
+var i    = undefined ;
+var j    = undefined ;
+var line = undefined ;
+
+console.log( '           e=          pt=          pe=          pn=           n=           s=           a=           b=          ns=          nf=          ng=         rrs=         rrf=         rrg=          rr=          rs=          rf=          rg=           r=' ) ;
+console.log( '          (1)          (2)          (3)          (4)          (5)          (6)          (7)          (8)          (9)         (10)         (11)         (12)         (13)         (14)         (15)         (16)         (17)         (18)         (19)' ) ;
+
+if( short )
+ 
+  for( i = 0 ; i < rs.length ; ++i , console.log( line ) )
+
+    for( line = '' , j = 0 ; j < rs[0].length ; ++j )
+
+      line += ( '  ' + rs[i][j].toExponential(6) ) ;
+     
+else
+
+  for( i = 0 ; i < rl.length ; ++i , console.log( line ) )
+
+    for( line = '' , j = 0 ; j < rl[0].length ; ++j )
+
+      line += ( '  ' + rl[i][j].toExponential(6) ) ;
