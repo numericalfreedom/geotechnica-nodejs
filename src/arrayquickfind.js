29ae03c42e124648c77d19da6955b6ad4eceb77c
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2020-03-27 11:44:25 +0100

    arrayquickfind.js modified

--- src/arrayquickfind.js
+++ src/arrayquickfind.js
@@ -2,9 +2,9 @@
 var i = undefined ;
 var a = new Array( 20 ) ;
 
-for( i = 0 ; i < a.length ; a[i] = i++ ) ;
+for( i = 0 ; i < a.length ; a[i] = ((i < 10) ? i++ : (1 + i++)) ) ;
 
-var v = 10 ;
+var v = 20 ;
 
 
 // quickfind( a , v ) ;
@@ -13,21 +13,25 @@ var v = 10 ;
 function quickfind( a , v )
  {
 
+  let ri = undefined ;
 
   let i  = undefined ;
   let nl = undefined ;
   let nr = undefined ;
 
-
   // for( nl = 0 , nr = (a.length - 1) , i = 0 ; a[i] != v; (i == 0) ? (i = nr) : (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) ) 
 
-  for( nl = 0 , nr = (a.length - 1) , i = Math.floor( (nl + nr) / 2 ) ; a[i] != v; (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) )
+  // for( nl = 0 , nr = (a.length - 1) , i = Math.floor( (nl + nr) / 2 ) ; (((a[i] != v) || (ri = i)) && ((nr-nl) > 1)) ; (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) )
+	 
+  for( nl = 0 , nr = (a.length - 1) , i = Math.floor( (nl + nr) / 2 ) ; (((a[i] != v) || (ri = i)) && ((nr-nl) > 1)) ; ((a[i] > v) || (nl = i)) , ((a[i] < v) || (nr = i)) , i = Math.floor( (nl + nr) / 2 ) ) 
 
     console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , a[i] ) ;
 
-  console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , a[i] ) ;
+//  if( a[i] == v )  ri = i ;  else  ri = undefined ;
+	 
+  console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , a[i] , "ri=" , ri ) ;
 
-  return( i ) ;
+  return( ri ) ;
 
  } ; // end for() -
 
@@ -36,6 +40,7 @@ function quickfind( a , v )
 function arrayquickfind( v )
  {
 
+  let ri = undefined ;
 
   let i  = undefined ;
   let nl = undefined ;
@@ -43,18 +48,21 @@ function arrayquickfind( v )
 
   // for( nl = 0 , nr = (a.length - 1) , i = 0 ; a[i] != v; (i == 0) ? (i = nr) : (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) ) 
 
-  for( nl = 0 , nr = (a.length - 1) , i = Math.floor( (nl + nr) / 2 ) ; a[i] != v; (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) )
+  for( nl = 0 , nr = (a.length - 1) , i = Math.floor( (nl + nr) / 2 ) ; ((a[i] != v) && ((nr-nl) > 2)) ; (((a[i] < v) ? (i = Math.floor( ((nl = i) + nr) / 2 )) : i ) , ((a[i] > v) ? (i = Math.floor( (nl + (nr = i)) / 2 )) : i)) )
 
     console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , this[i] ) ;
 
-  console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , this[i] ) ;
 
-  return( i ) ;
+  if( a[i] == v )  ri = i ;  else  ri = undefined ;
+	 
+  console.log( "nl=" , nl , "nr=" , nr , "i=" , i , "a[i]=" , a[i] , "ri=" , ri ) ;
+
+  return( ri ) ;
 
  } ; // end for() -
 
 Array.prototype.quickfind = arrayquickfind ;
 
-a.quickfind( v ) ;
+// a.quickfind( v ) ;
 
 quickfind( a , v ) ;
