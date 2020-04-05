90ce442a1b683390bc49382c5ffb3d2f3bfac720
Dr. Nandor Tamaskovics <numericalfreedom@googlemail.com>
2020-04-05 13:14:06 +0200

    lsdynamat.js updated.

--- src/cycliclsdynamat.js
+++ src/cycliclsdynamat.js
@@ -6,42 +6,84 @@ const l     = require( './lsdynamat' ) ;
 
 const ll    = new l.lsdynamat() ;
 
+const vemz  =    2.50e3 ;
+const wemz  =    0.55 ;
+const memz  =    0.00 ;
 
+const vrmz  =    1.50e5 ;
+const wrmz  =    0.00 ;
+const mrmz  =    0.00 ;
 
-const ptf   = 1.00e4 ;
+const rhos  = 2650.00 ;
+const fezz  =    3.00 ;
 
-const rhos  = 2650.0 ;
-const rhod  = 1900.0 ;
-
-const vemz  =  27.00 ;
-const wemz  =   0.60 ;
-const memz  =   0.00 ;
-
-const vrmz  =   5.00e3 ;
-const wrmz  =   0.00 ;
-const mrmz  =   0.00 ;
-
-
-const fezz  = 2.97 ;
-const ez    = ( (rhos / rhod) - 1.0 ) ;
-
+const ptf   =    1.00e4 ;
 const esc   = (- 1.0    ) ;
-const ssc   = (  1.0e-6 ) ;
+const ssc   = ( 0.500 * 1.0e-6 ) ;
 
-ll.nz       = ( ez / (1.0 + ez) ) ;
-ll.sz       = 0.0 ;
+var   rhod  = undefined ;
+var   ez    = undefined ;
+var   nz    = undefined ;
+
+var   pfn   = undefined ;
+
+
+const pts   = [ 0.00 , 1.00e7 , 0.00 , 2.00e7 , 0.00 , 4.00e7 , 0.00 , 6.00e7 , 0.00 ] ;
+
+const ezz   =    1.0 ;
+const mez   =    4.9 ;
+
+rhod        = 1900.0 ;
+ez          = ( (rhos / rhod) - 1.0 ) ;
+nz          = ( ez / (1.0 + ez) ) ;
+
+ll.nzs      = ( 1.0 - nz ) ;
+ll.nzf      = 0.0 ;
+ll.nzg      = nz ;
+
+console.log( "nzs=" , ll.nzs ) ;
+console.log( "nzf=" , ll.nzf ) ;
+console.log( "nzg=" , ll.nzg ) ;
 
 ll.vem      = ( (((fezz - ez) * (fezz - ez))) * vemz ) ;
+ll.vem      = ( vemz * Math.pow( ((ezz - ez) / ezz) , mez ) ) ;
 ll.wem      = wemz ;
 ll.mem      = memz ;
 
 ll.vrm      = ( (((fezz - ez) * (fezz - ez))) * vrmz ) ;
+ll.vrm      = ( vrmz * Math.pow( ((ezz - ez) / ezz) , mez ) ) ;
 ll.wrm      = wrmz ;
 ll.mrm      = mrmz ;
 
-const pfn   = 'Oed_V1_rho1900kgm3_sv60MPa_dedt10_4_model.dat' ;
-
-const pts   = [ 0.00 , 1.00e7 , 1.00e5 , 2.00e7 , 1.00e5 , 4.00e7 , 1.00e5 , 6.00e7 , 1.00e5 ] ;
+pfn         = 'Oed_V1_rho1900kgm3_sv60MPa_dedt10_4_model.dat' ;
 
 ll.cycliclsdynamat( pfn , pts , ptf , esc , ssc ) ;
+
+
+rhod        = 2000.0 ;
+ez          = ( (rhos / rhod) - 1.0 ) ;
+nz          = ( ez / (1.0 + ez) ) ;
+
+ll.nzs      = ( 1.0 - nz ) ;
+ll.nzf      = 0.0 ;
+ll.nzg      = nz ;
+
+console.log( "nzs=" , ll.nzs ) ;
+console.log( "nzf=" , ll.nzf ) ;
+console.log( "nzg=" , ll.nzg ) ;
+
+ll.vem      = ( (((fezz - ez) * (fezz - ez))) * vemz ) ;
+ll.vem      = ( vemz * Math.pow( ((ezz - ez) / ezz) , mez ) ) ;
+ll.wem      = wemz ;
+ll.mem      = memz ;
+
+ll.vrm      = ( (((fezz - ez) * (fezz - ez))) * vrmz ) ;
+ll.vrm      = ( vrmz * Math.pow( ((ezz - ez) / ezz) , mez ) ) ;
+ll.wrm      = wrmz ;
+ll.mrm      = mrmz ;
+
+pfn         = 'Oed_V5_rho2000kgm3_sv60MPa_dedt10_4_model.dat' ;
+
+ll.cycliclsdynamat( pfn , pts , ptf , esc , ssc ) ;
+
 
